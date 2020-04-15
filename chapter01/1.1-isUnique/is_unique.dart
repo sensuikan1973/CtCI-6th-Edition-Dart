11767f5ffeb375008b7fe60ba28bac7bffc11304
ShimizuNaoki <sensuikan1973@gmail.com>
2020-04-15 23:13:31 +0900

    improve 1.1 performance

--- chapter01/1.1-isUnique/is_unique.dart
+++ chapter01/1.1-isUnique/is_unique.dart
@@ -1,4 +1,4 @@
 bool isUnique(String str) {
   if (str.length > 256) return false;
-  return Set.from(str.codeUnits).length == str.length;
+  return Set<int>.of(str.codeUnits).length == str.length;
 }
